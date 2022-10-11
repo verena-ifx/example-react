cf92f2f0ab01c4e5c04ac9304d61836d7d28a063
Verena Lechner <lechneve@ISCN5CG1201S7M.infineon.com>
2022-10-11 10:52:31 +0200

    working with ifx bootstrap now

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button type="button" class="btn btn-primary" >Test Button</button>
+
         <a
           className="App-link"
           href="https://reactjs.org"
